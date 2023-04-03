f1b66cb408917fca0fc3e2173d0af0144f771d98
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-04-03 13:51:20 +0200

    Nav

--- app/src/main/java/com/example/enidemo/MainActivity.kt
+++ app/src/main/java/com/example/enidemo/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.enidemo.databinding.ActivityMainBinding
+import com.example.enidemo.fragment.BlankFragment
+import com.example.enidemo.fragment.BlankFragmentDirections
 import java.util.*
 import kotlin.concurrent.schedule
 
@@ -17,16 +19,33 @@ class MainActivity : AppCompatActivity() {
 
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+        /*
         Timer().schedule(1000){
             //do something
             onClick();
         }
-
+    */
+        onClick();
     }
 
     fun onClick() {
-        // get nav controller
-        val navController = findNavController(R.id.navExample).navigate(R.id.action_blankFragment_to_twoFragment)
+        // Naviguer sans arguments
+        //findNavController(R.id.navExample).navigate(R.id.action_blankFragment_to_twoFragment)
+
+        // Naviguer avec arguments
+        // Methode 1
+        /*
+        var action = BlankFragmentDirections.actionBlankFragmentToTwoFragment();
+        action.arguments.putInt("code",10)
+        action.arguments.putString("nom", "Test")
+        */
+
+        // Methode 2
+        var action = BlankFragmentDirections.actionBlankFragmentToTwoFragment();
+        action.nom = "test"
+        action.code = 20
+        findNavController(R.id.navExample).navigate(action)
+
     }
 
 }
--- app/src/main/java/com/example/enidemo/fragment/TwoFragment.kt
+++ app/src/main/java/com/example/enidemo/fragment/TwoFragment.kt
@@ -7,27 +7,22 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.enidemo.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [TwoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class TwoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        // Comment récupérer les arguments
+        val args = arguments?.let { TwoFragmentArgs.fromBundle(it) }
+        val name = args?.nom
+        val code = args?.code
+
+        println(String.format("%s => %s", name, code))
     }
 
     override fun onCreateView(
@@ -36,25 +31,5 @@ class TwoFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_two, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TwoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TwoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
