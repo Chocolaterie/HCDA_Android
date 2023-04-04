d0b30fa565b95ddf8e0262ac05124f5e196197b6
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-04-04 14:25:38 +0200

    Demo compteur

--- app/src/main/java/com/example/enidemo/counter/CounterActivity.kt
+++ app/src/main/java/com/example/enidemo/counter/CounterActivity.kt
@@ -3,11 +3,16 @@ package com.example.enidemo.counter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.example.enidemo.R
 import com.example.enidemo.databinding.ActivityCounterBinding
 import com.example.enidemo.databinding.ActivityMainBinding
 
 class CounterActivity : AppCompatActivity() {
+
+    // ViewModel c'est un membre de classe
+    lateinit var counterViewModel: CounterViewModel;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -15,7 +20,16 @@ class CounterActivity : AppCompatActivity() {
         val activityCounterBinding = DataBindingUtil.setContentView<ActivityCounterBinding>(this, R.layout.activity_counter)
 
         // Instancier par défaut le view model
-        activityCounterBinding.counterViewModel = CounterViewModel()
+        counterViewModel = CounterViewModel()
 
+        // Mettre à jour le view model dans la vue
+        activityCounterBinding.counterViewModel = counterViewModel
+
+        // Ecouter le changement du compteur
+        counterViewModel.counter.observe(this, Observer {
+            // Que faire lorsque je detecte un changement dans le compteur
+            // Mettre à jour le view model dans la vue
+            activityCounterBinding.counterViewModel = counterViewModel
+        })
     }
 }
--- app/src/main/java/com/example/enidemo/counter/CounterViewModel.kt
+++ app/src/main/java/com/example/enidemo/counter/CounterViewModel.kt
@@ -1,12 +1,30 @@
 package com.example.enidemo.counter
 
+import android.view.View
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CounterViewModel : ViewModel() {
 
-    var counter = 5
+    // le compteur est observable
+    var counter = MutableLiveData<Int>()
 
+    // Constructuer
+    init {
+        counter.value = 5
+    }
+
+    // Fonctionne pour modifier la valeur du compteur
     fun addCounter(){
-        counter++;
+        // Les deux fonctionnent
+        // -- incrementer
+        counter.value = (counter.value)?.plus(1)
+        // -- set
+        // counter.value = 20;
+    }
+
+    // Evenement clique
+    fun onClickAddCounter(view: View){
+        addCounter();
     }
 }
